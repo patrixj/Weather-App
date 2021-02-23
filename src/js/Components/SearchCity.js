eac26741c1667abdd80da0183e96f6f62422f8c4
patrixj <Patrik.Janostak@seznam.cz>
2021-02-23 20:24:42 +0100

    hide hint

--- src/js/Components/SearchCity.js
+++ src/js/Components/SearchCity.js
@@ -1,50 +1,56 @@
 export class SearchCity {
-    constructor() {
-        this.jsonUrl = "city.list.json";
-        this.container = document.createElement("div");
-        this.input = document.createElement("input");
-        this.input.type = "text";
-        this.input.placeholder = "Search(min 3 characters)"
-        this.input.addEventListener("input", this.filterCities.bind(this));
-        this.input.addEventListener("keyup", this.onEnter.bind(this));
-        this.whisperer = document.createElement("div");
-        this.whisperer.classList.add("whisperer");
-        this.container.appendChild(this.input);
-        this.container.appendChild(this.whisperer);
-    }
+  constructor() {
+    this.jsonUrl = "city.list.json";
+    this.container = document.createElement("div");
+    this.input = document.createElement("input");
+    this.input.type = "text";
+    this.input.placeholder = "Search(min 3 characters)";
+    this.input.addEventListener("input", this.filterCities.bind(this));
+    this.input.addEventListener("keyup", this.onEnter.bind(this));
+    this.whisperer = document.createElement("div");
+    this.whisperer.classList.add("whisperer");
+    this.container.appendChild(this.input);
+    this.container.appendChild(this.whisperer);
+  }
 
-    async init(onRowClick) {
-        this.data = await (await fetch(this.jsonUrl)).json();
-        this.onRowClick = onRowClick;
-    }
+  async init(onRowClick) {
+    this.data = await (await fetch(this.jsonUrl)).json();
+    this.onRowClick = onRowClick;
+  }
 
-    filterCities(event) {
-        const value = event.target.value;
+  filterCities(event) {
+    const value = event.target.value;
+    this.whisperer.innerHTML = "";
+
+    if (!value || value.length < 3) return;
+
+    const matched = this.data
+      .filter((d) =>
+        d.name.toLocaleLowerCase().startsWith(value.toLocaleLowerCase())
+      )
+      .sort((d1, d2) => d1.name.length - d2.name.length);
+    for (const match of matched) {
+      const row = document.createElement("div");
+      row.textContent = match.name + `(${match.country})`;
+      this.whisperer.appendChild(row);
+      row.addEventListener("click", (e) => {
         this.whisperer.innerHTML = "";
-
-        if (!value || value.length < 3)
-            return;
-
-        const matched = this.data.filter(d => d.name.toLocaleLowerCase().startsWith(value.toLocaleLowerCase()))
-            .sort((d1, d2) => d1.name.length - d2.name.length);
-        for (const match of matched) {
-            const row = document.createElement("div");
-            row.textContent = match.name + `(${match.country})`;
-            this.whisperer.appendChild(row);
-            row.addEventListener("click", e => {
-                const prev = this.whisperer.querySelector(".selected");
-                if (prev)
-                    prev.classList.remove("selected");
-                row.classList.add("selected");
-                this.input.value = row.textContent;
-                this.onRowClick(match);
-            });
-        }
+        const prev = this.whisperer.querySelector(".selected");
+        if (prev) prev.classList.remove("selected");
+        row.classList.add("selected");
+        this.input.value = row.textContent;
+        this.onRowClick(match);
+      });
     }
+  }
 
-    onEnter(event) {
-        if (event.key == "Enter" && this.input.value && this.whisperer.childNodes.length > 0) {
-            this.whisperer.childNodes[0].click();
-        }
+  onEnter(event) {
+    if (
+      event.key == "Enter" &&
+      this.input.value &&
+      this.whisperer.childNodes.length > 0
+    ) {
+      this.whisperer.childNodes[0].click();
     }
+  }
 }
